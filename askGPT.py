0bb8be671312226ab893f8b9d7005330d1281ee2
Delicate2000 <71369535+Delicate2000@users.noreply.github.com>
2024-01-25 01:11:38 +0800

    Add files via upload

--- askGPT.py
+++ askGPT.py
@@ -46,75 +46,24 @@ def askGPT_1round(instruction, user_content):
             {"role": "system", "content": instruction},
             {"role": "user", "content": user_content},
             ],
-        temperature=0,  #0.0 to 2.0 (默认 1.0) 温度，越高越随机，越低越有规律（或确定性）。
-        # top_p=1,  # 0.0~1.0(默认1.0)不要跟温度同时用, top_p表示只考虑概率最高的top_p token，如 top_p=0.1，表示模型只考虑概率最高的 10% 的 token。
-        # n=1,  #number (默认 1) 生成的回复数量.
-        # stream=False,  # boolean (default False)
-        # stop=None,  # string or array (default None)
-        # presence_penalty=0,  # -2.0 to 2.0 (default 0)
-        # frequency_penalty=0,  # -2.0 to 2.0 (default 0)
-        # max_tokens=10,  # inf (default 4096-prompt_token)
-        # logit_bias=
-        # user=
+        temperature=0, 
     )
     return completion.choices[0].message.content
 
 
 def askGPT_meta(messages):
-    """role：有效值为“system”、“user”、“assistant”
-       content
-    """
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-16k-0613",
-        # model="gpt-3.5-turbo-1106",
-        # model="text-davinci-003",
-        
         messages=messages,
         temperature=0,
-        seed=42,
     )
     return completion.choices[0].message.content
 
 
-# key_list = keys.split('\n')
-# keyQueue = KEYQueue(key_list)
-# openai.api_key = keyQueue.getKey()
-
-# def askGPT4Use_nround(messages):
-#     res = 'None'
-#     status_code = 0
-#     for _ in range(10):
-#         status_code += 1
-#         if len(keyQueue.lookCurrQueue()) < 3:
-#             print('api_key池不足3个！！！')
-#         try:
-#             time.sleep(2)
-#             res = askGPT_meta(messages)
-#             status_code -= 1
-#             break
-#         except RateLimitError as e:
-#             if any(sign in e._message for sign in ChangeKey_message):
-#                 # print('更换key')
-#                 openai.api_key = keyQueue.getKey()
-#             elif e._message in DelKey_message:
-#                 # print('删除key, 更换key')
-#                 keyQueue.delKey(openai.api_key)
-#                 openai.api_key = keyQueue.getKey()
-#             else:
-#                 print(e)
-#                 time.sleep(20)
-#         except Exception as e:
-#             print(e)
-#             time.sleep(20)
-#     if status_code == 10:
-#         print('失败')
-#     return res
-
-
 class ASK_GPT(object):
     def __init__(self, keys, time_sleep=2, error_sleep=20):
-        self.time_sleep = time_sleep # 正常两次请求之间的sleep间隔
-        self.error_sleep = error_sleep # 发生错误后的sleep间隔
+        self.time_sleep = time_sleep 
+        self.error_sleep = error_sleep 
 
         if isinstance(keys, str): # keys:使用"\n"来分割key
             key_list = keys.split('\n')
@@ -131,9 +80,8 @@ class ASK_GPT(object):
         res = 'None'
         status_code = 0
         for _ in range(10):
-            status_code += 1 # status_code指示正处于第几轮循环
+            status_code += 1 
             if len(self.keyQueue.lookCurrQueue()) < 3:
-                # print('api_key池不足3个！！！')
                 pass
             try:
                 time.sleep(self.time_sleep)
@@ -142,10 +90,8 @@ class ASK_GPT(object):
                 break
             except RateLimitError as e:
                 if any(sign in e._message for sign in ChangeKey_message):
-                    # print('更换key')
                     openai.api_key = self.keyQueue.getKey()
                 elif e._message in DelKey_message:
-                    # print('删除key, 更换key')
                     self.keyQueue.delKey(openai.api_key)
                     openai.api_key = self.keyQueue.getKey()
                 else:
@@ -155,7 +101,7 @@ class ASK_GPT(object):
                 print(e)
                 time.sleep(self.error_sleep)
         if status_code == 10:
-            print('失败')
+            print('Fail')
         return res
 
 
